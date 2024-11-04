7e135c8c7136f9023b4b2a591574e125e2e9822a
sshduck <d00479329@utahtech.edu>
2024-11-03 19:31:26 -0700

    Implemented a class for simpler function

--- RPS.py
+++ RPS.py
@@ -1,34 +1,44 @@
 import random
 
+class Choices:
+    def __init__(self, userchoice, botchoice):
+        self.userchoice = userchoice.lower()
+        self.botchoice = botchoice
+
+    def check(self):
+        if self.userchoice == self.botchoice:
+            print("Jinx! Retry.")
+        if self.userchoice not in "rock, paper, scissors":
+            print("Invalid string. Try again!")
+
+        if self.userchoice.lower() == "rock" and self.botchoice == "paper":
+            print("Paper! You lose! Retry.")
+        if self.userchoice.lower() == "rock" and self.botchoice == "scissors":
+            print("Scissors! You win!")
+
+        if self.userchoice.lower() == "paper" and self.botchoice == "scissors":
+            print("Scissors! You lose! Retry.")
+        if self.userchoice.lower() == "paper" and self.botchoice == "rock":
+            print("Rock! You win!")
+
+        if self.userchoice.lower() == "scissors" and self.botchoice == "rock":
+            print("Rock! You lose! Retry.")
+        if self.userchoice.lower() == "scissors" and self.botchoice == "paper":
+            print("Paper! You win!")
+
+
+
 def RPS():
     print("Let's play Rock, Paper, Scissors!")
     
     choices = ["rock", "paper", "scissors"]
 
-    random.shuffle(choices)
-    botchoice = choices[0]
-    userchoice = input("What option are you throwing? ")
+    botchoice = random.choice(choices)
+    userchoice = input("What option are you throwing? ").strip()
 
-    if userchoice == botchoice:
-        print("Jinx! Retry.")
-    if userchoice not in "rock, paper, scissors":
-        print("Invalid string. Try again!")
+    go = Choices(userchoice, botchoice)
+    go.check()
 
-    if userchoice.lower() == "rock" and botchoice == "paper":
-        print("Paper! You lose! Retry.")
-    if userchoice.lower() == "rock" and botchoice == "scissors":
-        print("Scissors! You win!")
-
-    if userchoice.lower() == "paper" and botchoice == "scissors":
-        print("Scissors! You lose! Retry.")
-    if userchoice.lower() == "paper" and botchoice == "rock":
-        print("Rock! You win!")
-
-    if userchoice.lower() == "scissors" and botchoice == "rock":
-        print("Rock! You lose! Retry.")
-    if userchoice.lower() == "scissors" and botchoice == "paper":
-        print("Paper! You win!")
-
-
+   
 if __name__ == "__main__":
     RPS()
